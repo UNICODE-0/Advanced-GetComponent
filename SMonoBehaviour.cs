ca1f8ecad30fa063cb6fb1a1350f52a4fdedf3db
UNICODE-0 <82433896+UNICODE-0@users.noreply.github.com>
2024-03-27 12:30:02 +0300

    Update SMonoBehaviour.cs

--- SMonoBehaviour.cs
+++ SMonoBehaviour.cs
@@ -6,19 +6,40 @@ using Debug = UnityEngine.Debug;
 public class SMonoBehaviour : MonoBehaviour
 {
     private const string ROOT_TAG = "Root";
-    private static Dictionary<Type, Dictionary<int, List<dynamic>>> shared = new();
+    private static Dictionary<Type, Dictionary<int, List<SMonoBehaviour>>> shared = new();
 
     private Type _type = null;
     private GameObject _rootGameObject = null;
     private int _instanceID = -1;
 
+    public static bool TryGetSharedComponent<T>(int instanceID, out T component) where T: SMonoBehaviour
+    {
+        component = GetSharedComponent<T>(instanceID);
+        
+        if(component == null) return false;
+        else return true;
+    } 
     public static T GetSharedComponent<T>(int instanceID) where T: SMonoBehaviour
     {
-        return shared[typeof(T)][instanceID][0] as T;
+        try
+        {
+            return shared[typeof(T)][instanceID][0] as T;
+        }
+        catch (Exception)
+        {
+            return null;
+        }
     } 
-    public static List<T> GetSharedComponents<T>(int instanceID) where T: SMonoBehaviour
+    public static List<T> GetSharedComponents<T>(int instanceID) where T: SMonoBehaviour 
     {
-        return shared[typeof(T)][instanceID] as List<T>;
+        try
+        {
+            return shared[typeof(T)][instanceID] as List<T>;
+        }
+        catch (Exception)
+        {
+            return null;   
+        }
     } 
     private void InitRootGameObject()
     {
@@ -52,17 +73,17 @@ public class SMonoBehaviour : MonoBehaviour
         _type = this.GetType();
         if(!shared.ContainsKey(_type))
         {
-            shared.Add(_type, new Dictionary<int, List<dynamic>>());
+            shared.Add(_type, new Dictionary<int, List<SMonoBehaviour>>());
         }
 
         int id = _rootGameObject.GetInstanceID();
 
         if(!shared[_type].ContainsKey(id))
         {
-            shared[_type].Add(id, new List<dynamic>());
+            shared[_type].Add(id, new List<SMonoBehaviour>());
         }
 
-        shared[_type][id].Add(Convert.ChangeType(this, _type));
+        shared[_type][id].Add(this);
     }
     private void RemoveComponent()
     {
